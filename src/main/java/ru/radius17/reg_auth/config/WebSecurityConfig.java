fb6a9a27b1707f2e3ad3825e07ef950e41e9298b
Radius17 <radius17@gmail.com>
2022-07-14 09:15:34 +0300

    Подключение авторизации и страница логина.

---
+++ src/main/java/ru/radius17/reg_auth/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package ru.radius17.reg_auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable();
+        //Доступ только для не зарегистрированных пользователей
+        httpSecurity.authorizeRequests().antMatchers("/registration").not().fullyAuthenticated();
+        //Доступ только для пользователей с ролью Администратор
+        httpSecurity.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+        //Доступ только для пользователей с ролью Пользователь
+        httpSecurity.authorizeRequests().antMatchers("/profile").hasRole("USER");
+        //Доступ разрешен всем подряд
+        httpSecurity.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**").permitAll();
+        //Все остальные страницы требуют аутентификации
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+        //Настройка для входа в систему и перенарпавление на главную страницу после успешного входа
+        httpSecurity.formLogin().loginPage("/login").defaultSuccessUrl("/").permitAll();
+        // Настройки для выхода из системы
+        httpSecurity.logout().permitAll().logoutSuccessUrl("/").deleteCookies("JSESSIONID");
+
+        return httpSecurity.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
